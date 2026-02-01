d48f5cf6eafac785e0092aa83700191c2f7cb6d2
hegazy <ahmedrady03@gmail.com>
2026-02-01 19:27:29 +0200

    feat: Integrate Firebase configuration and options for Android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flosy/core/theme/app_theme.dart';
-import 'package:flosy/features/auth/screens/cubit/auth_cubit_cubit.dart';
-import 'package:flosy/features/auth/screens/login_screen.dart';
+import 'package:flosy/firebase_options.dart';
 import 'package:flosy/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final AppTheme appTheme = AppTheme();
 
