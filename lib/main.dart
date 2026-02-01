692f1edbb32937576bd27c98c070e51682aa12c7
hegazy <ahmedrady03@gmail.com>
2026-02-01 23:49:40 +0200

    feat: Add Firebase configuration and update main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flosy/core/theme/app_theme.dart';
 import 'package:flosy/features/auth/screens/cubit/auth_cubit_cubit.dart';
+import 'package:flosy/firebase_options.dart';
 import 'package:flosy/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
